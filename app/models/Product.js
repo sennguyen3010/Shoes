d044e8b6a52008a4b208ec16fd5c627429aefb65
nguyenthikimsen <kimsen3077@gmail.com>
2022-07-26 21:44:53 +0700

    api productFeature

---
+++ app/models/Product.js
@@ -0,0 +1,15 @@
+export class Product {
+  id = '';
+  name = '';
+  alias = '';
+  price = '';
+  description = '';
+  size = '';
+  shortDescription = '';
+  quantity = '';
+  deleted = '';
+  categories = '';
+  relatedProducts = '';
+  feature = '';
+  image = '';
+}
